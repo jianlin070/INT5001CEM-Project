5bd2aac56512db9f6756e4a29094474943682886
ZhengHoom <ZhengHoom@MSI>
2022-10-16 00:13:18 +0800

    -bind files

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -1,20 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package project;
+package project2;
 
 /**
  *
  * @author jianlin070
  */
 public class Project {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
